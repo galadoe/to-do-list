e7d03faaa84cd1287952567f47902fda946e5711
galadoe <kgaladoe@gmail.com>
2021-08-10 21:22:08 -0700

    almost done with todo list

--- todo-list/app.js
+++ todo-list/app.js
@@ -3,7 +3,7 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
-const filterOption = document.querySelector('.filter-todo')
+const filterOption = document.querySelector('.filter-todo');
 
 
 //Event Listerners
